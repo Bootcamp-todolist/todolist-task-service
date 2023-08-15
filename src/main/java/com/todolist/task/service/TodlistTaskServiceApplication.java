1622a9f283bd1d537712d89752b05a52c67f4d11
kexin.an <kexin.an@thoughtworks.com>
2023-08-15 10:08:41 +0800

    init

---
+++ src/main/java/com/todolist/task/service/TodlistTaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.task.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodlistTaskServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodlistTaskServiceApplication.class, args);
+  }
+
+}
